2831f5e993b41a2df70579438fc25d976ccd8b95
darcy-bcit <D'Arcy_D_Smith@bcit.ca>
2021-07-13 16:51:12 -0700

    more bit fiddling

---
+++ bit-differences/main.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <arpa/inet.h>
+
+
+// warning - make sure you are shifting with unsigned values only!
+int main(void)
+{
+    unsigned char byteA;
+    unsigned char byteB;
+    unsigned char differences;
+
+    // 01001100 = 76
+    // 00011001 = 25
+    // --------
+    // 01010101 = 85
+    byteA       = 76;
+    byteB       = 25;
+    differences = byteA ^ byteB;
+    printf("%u\n", byteA);
+    printf("%u\n", byteB);
+    printf("%u\n", differences);
+
+    return EXIT_SUCCESS;
+}
---
+++ bit-same/main.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <arpa/inet.h>
+
+
+// warning - make sure you are shifting with unsigned values only!
+int main(void)
+{
+    unsigned char byte;
+    unsigned char mask;
+    unsigned char masked;
+
+    // 11011111 = 223
+    // 11110000 = 240
+    // --------
+    // 11010000 = 208
+    byte   = 223;
+    mask   = 240;
+    masked = byte & mask;
+    printf("%u\n", byte);
+    printf("%u\n", mask);
+    printf("%u\n", masked);
+
+    return EXIT_SUCCESS;
+}
