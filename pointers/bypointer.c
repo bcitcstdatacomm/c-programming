9626f9af64418c0485ea30f6bd8cf6a240fde181
darcy-bcit <D'Arcy_D_Smith@bcit.ca>
2021-07-13 17:56:54 -0700

    pointers

---
+++ pointers/bypointer.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+static void display(int *value);
+
+
+int main(void)
+{
+    int a;
+
+    a = 5;
+    printf("[%p] = %d\n", (void *)&a, a);
+    display(&a);
+    printf("[%p] = %d\n", (void *)&a, a);
+
+    return EXIT_SUCCESS;
+}
+
+static void display(int *value)
+{
+    printf("[%p] = %p\n", (void *)&value, value);
+    printf("[%p] = %d\n", (void *)value, *value);
+    *value = 6;
+}
---
+++ pointers/byvalue.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+static void display(int value);
+
+
+int main(void)
+{
+    int a;
+
+    a = 5;
+    printf("[%p] = %d\n", (void *)&a, a);
+    display(a);
+    printf("[%p] = %d\n", (void *)&a, a);
+
+    return EXIT_SUCCESS;
+}
+
+static void display(int value)
+{
+    printf("[%p] = %d\n", (void *)&value, value);
+    value = 6;
+}
