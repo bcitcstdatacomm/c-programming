fc2f2f4339bcab2d37e1cef860ab601908f72699
darcy-bcit <D'Arcy_D_Smith@bcit.ca>
2021-07-13 19:56:07 -0700

    added memory address example

---
+++ memory-addresses/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+static int y;
+
+int main(void)
+{
+    static int x;
+    char a;
+    char b;
+    char c;
+    char d;
+    char e;
+
+    printf("x = [%p]\n", (void *)&y);
+    printf("x = [%p]\n", (void *)&x);
+    printf("a = [%p]\n", (void *)&a);
+    printf("b = [%p]\n", (void *)&b);
+    printf("c = [%p]\n", (void *)&c);
+    printf("d = [%p]\n", (void *)&d);
+    printf("e = [%p]\n", (void *)&e);
+    printf("[%p]\n", (void *)"Hello, World!");
+
+    return EXIT_SUCCESS;
+}
